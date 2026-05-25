3a40ab54306cffe70b30a35802f6556f72b080b7
Mehmet Emin AKPOLAT <AkpolatEmin01@gmail.com>
2026-05-25 04:39:02 +0300

    Add project files.

---
+++ EngilishCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EngilishCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
